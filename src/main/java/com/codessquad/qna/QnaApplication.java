23f0c5b6ddacd7f17771a8ff74f5b61875987883
jeong-inho <derosatam76@gmail.com>
2021-03-27 21:42:49 +0900

    Feat : QnaApplication class에 customize 메소드 생성
    페이지의 인덱스를 0부터 시작이 아닌 1부터 시작으로 변경하기 위해 PageableHandlerMethodArgumentResolverCustomizer를 반환하는 customize 메소드를 만들고 @Bean 설정

--- src/main/java/com/codessquad/qna/QnaApplication.java
+++ src/main/java/com/codessquad/qna/QnaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -18,6 +19,11 @@ public class QnaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QnaApplication.class, args);
+	}
+
+	@Bean
+	public PageableHandlerMethodArgumentResolverCustomizer customize() {
+		return p -> p.setOneIndexedParameters(true);
 	}
 
 	@Bean
